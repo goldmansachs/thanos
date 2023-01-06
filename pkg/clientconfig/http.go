73309ae99a2e218689b3c1d4c2c628af683131eb
Radek Ryckowski <radek.ryckowski@gmail.com>
2023-01-06 10:51:13 +0000

    fix Bearer token
    Signed-off-by: Radek Ryckowski <radek.ryckowski@gmail.com>

--- pkg/clientconfig/http.go
+++ pkg/clientconfig/http.go
@@ -169,7 +169,7 @@ func NewRoundTripperFromConfig(cfg config_util.HTTPClientConfig, transportConfig
 		// Backwards compatibility, be nice with importers who would not have
 		// called Validate().
 		if len(cfg.BearerToken) > 0 {
-			rt = config_util.NewAuthorizationCredentialsRoundTripper("Bearer", config_util.NewInlineSecret(string(cfg.BearerToken)), rt)
+			rt = config_util.NewAuthorizationCredentialsRoundTripper("Bearer", config_util.Secret(cfg.BearerToken), rt)
 		} else if len(cfg.BearerTokenFile) > 0 {
 			rt = config_util.NewAuthorizationCredentialsRoundTripper("Bearer", config_util.NewFileSecret(cfg.BearerTokenFile), rt)
 		}
