1c49226fba8fd236b5765f814596c38c101b4723
Radek Ryckowski <radek.ryckowski@gmail.com>
2023-07-03 13:07:17 +0100

    diable warrning error as it increasing logs size
    Signed-off-by: Radek Ryckowski <radek.ryckowski@gmail.com>

--- pkg/store/proxy_heap.go
+++ pkg/store/proxy_heap.go
@@ -541,8 +541,8 @@ func newAsyncRespSet(
 
 	var labelsToRemove map[string]struct{}
 	if !st.SupportsWithoutReplicaLabels() && len(req.WithoutReplicaLabels) > 0 {
-		level.Warn(logger).Log("msg", "detecting store that does not support without replica label setting. "+
-			"Falling back to eager retrieval with additional sort. Make sure your storeAPI supports it to speed up your queries", "store", st.String())
+		//level.Warn(logger).Log("msg", "detecting store that does not support without replica label setting. "+
+		//	"Falling back to eager retrieval with additional sort. Make sure your storeAPI supports it to speed up your queries", "store", st.String())
 		retrievalStrategy = EagerRetrieval
 
 		labelsToRemove = make(map[string]struct{})
